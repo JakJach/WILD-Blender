8481f2fcdacce8996f4fa237998d2257845ad07c
Jakub <jakub.jachow@gmail.com>
2023-09-19 23:15:57 +0200

    Movew MainWindow to Views folder

---
+++ WILD.Blender/WILD.Blender/Views/MainWindowView.xaml.cs
@@ -0,0 +1,16 @@
+﻿using NLog;
+using System.Windows;
+
+namespace WILD.Blender.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindowView : Window
+    {
+        public MainWindowView()
+        {
+            InitializeComponent();
+        }
+    }
+}
